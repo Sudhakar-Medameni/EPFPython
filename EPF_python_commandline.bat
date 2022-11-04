30a2b2aeef5226dcdaf9b8ea20ebd2894dc37ddf
Sudhakar-Medameni <101382240+Sudhakar-Medameni@users.noreply.github.com>
2022-11-04 15:05:43 +0530

    Update EPF_python_commandline.bat

--- EPF_python_commandline.bat
+++ EPF_python_commandline.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Eggplant\runscript.bat" "E:\Eggplant\EggplanPracticesuite.suite\Scripts\Flipkart OCR readtext.script" -commandLineOuput yes
+"C:\Program Files\Eggplant\runscript.bat" "E:\Eggplant\EggplanPracticesuite.suite\Scripts\Flipkart OCR readtext.script" -CommandLineOutput yes
